0f28f336322b3c713488f464c0861352dd2ba619
Austin Vaughan <99289671+TemporaryPlaceholderAccount@users.noreply.github.com>
2022-02-27 23:39:09 -0600

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,31 @@
+
+print("Hello Python World")
+
+# NameError: name 'prinlt' is not defined
+#You can always make typos that creaate errors, simple misspellings and dropped characters are the single greatest cause of frustration and anger in every Software engineer I've met.
+#Making sense of the error message is usually easy when you get to the actual error, see above. It usually spells out the problem pretty clearly. The REAL issue is when the code runs but doesnt do what you want and you have no obvious idea why
+#Any typo in the actual print statement or a comment wouldnt cause an error, beecausee thee systeem doeesnt neeeed to definee thee teerms, see above and words preceding comma, and it's up to human observers to assign value to the characters.
+
+import this
+
+#The Zen of python is a good practice philosophy to keep code simple, usable, and clean. 
+
+message = "This is the second message"
+
+#This is the variable my message is stored in, im not sure how to show the fact that i changed it like the exercice said
+
+print(message)
+
+def display_message():
+
+    #This is a print statement explaining what I've learned this chapter
+
+    print ("I learned the fundamentals that I will use most often when coding with Python. I hope to continue honing my skills.")
+
+display_message()
+
+def favorite_book(book):
+
+    print(book.title() + " is my favorite book!")
+
+favorite_book("Eragon")
