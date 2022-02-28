39ce57acf672de7f40e70cf870537bd334f90e78
Austin Vaughan <99289671+TemporaryPlaceholderAccount@users.noreply.github.com>
2022-02-28 01:48:12 -0600

    Add files via upload

---
+++ Exercise_2.py
@@ -0,0 +1,38 @@
+def simple():
+
+    message1 = "1"
+    message2 = "2"
+    message3 = "3"
+    message4 = "4"
+    message5 = "5!"
+
+    print(message1, message2, message3, message4, message5)
+
+simple()
+
+def simple2():
+
+    message1v2 = "1a"
+
+    print(message1v2)
+
+    message1v2 = "1b"
+
+    print(message1v2)
+    
+    message1v2 = "1c"
+
+    print(message1v2)
+
+    message1v2 = "1d"
+
+    print(message1v2)
+
+simple2()
+
+def message(seashore):
+
+   print("Sally sold sea shells by the " + seashore)
+
+
+message("seashore")
