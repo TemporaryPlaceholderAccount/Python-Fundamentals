2a5797dd2dbc87e86b610fc6426ba9ed73018a6c
Austin Vaughan <99289671+TemporaryPlaceholderAccount@users.noreply.github.com>
2022-02-28 22:49:31 -0600

    Add files via upload

---
+++ Exercise4.py
@@ -0,0 +1,41 @@
+print(4+4)
+
+print(12-4)
+
+print(4*2)
+
+print(32/4)
+
+FN = "3773"
+
+print("My favorite number is " + FN)
+
+a = "456234_"
+b = "68423791_"
+c = "13794628_"
+d = "96374"
+
+def number_sets():
+
+    print(a + b + c + d)
+
+number_sets()
+
+def func1(i2f, f2i):
+
+    i2f = float(i2f)
+    f2i = int(f2i)
+  
+    print(i2f, f2i)
+
+func1(3, 5.5)
+
+def favmov(movie, watch):
+
+    movie = input("What is your favorite movie? ")
+
+    watch = input("Wow really!? And how many times have you seen it? ")
+
+    print("I have seen " + movie + watch + " times.")
+
+favmov("Pirates of the carribean", " 5")
