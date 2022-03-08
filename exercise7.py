9178dc468fb95ca37dfeafab0b9092a80024eaef
Austin Vaughan <99289671+TemporaryPlaceholderAccount@users.noreply.github.com>
2022-03-07 18:01:26 -0600

    Add files via upload

---
+++ exercise7.py
@@ -0,0 +1,22 @@
+
+#4-3
+for value in range(1,21):
+ print(value)
+
+ #4-6
+odds = range(1, 21, 2)
+
+for value in odds:
+    print(value)
+
+#4-7
+trips = range(3, 33, 3)
+
+for value in trips:
+    print(value)
+
+#4-8
+cubes = [1, 2**3, 3**3, 4**3, 5**3, 6**3, 7**3, 8**3, 9**3, 10**3]
+
+for value in cubes:
+    print(value)
